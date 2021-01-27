b93baa3885f0984bcfaebd9f7fe791257be0ad62
Mike Marino <marino1820@gmail.com>
2021-01-27 14:11:48 -0500

    Finished JS for color coding

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,31 +1,77 @@
 $(document).ready(function () {
 
 
-function renderNotes (){
-    var hourBlock = $("div.hour");
-    var storedNote = JSON.parse(localStorage.getItem('note'));
-    var existingNote = storedNote.find(({time}) => time === hourBlock);
-    
-    if(existingNote) {
-        console.log("Yes");        
+
+
+
+now = moment().format('dddd, MMMM Do');
+$("#currentDay").append().text(now)
+
+
+
+    function renderNotes() {
+        var hourBlock = $(".hour");
+        var storedNote = JSON.parse(localStorage.getItem('note'));
+
+         const oldNote1 = JSON.parse(localStorage.getItem('note')) || [];
+        // console.log(hourBlock)
+
+        hourBlock.each(function () {
+            hour1 = $(this).text();
+           
+            const existingItem1 = oldNote1.find(({time}) => time === hour1);
+            
+            if (existingItem1) {
+                                
+                loadText = existingItem1.text;
+             
+                $(this).next().text(loadText)
+                // console.log($(this).next())
+                // loadText= hourBox;
+            }
+            console.log("#########")
+        
+        })
     }
+
+setCurrentTimeColor()   
+function setCurrentTimeColor(){
+
+    var hourBlock = $(".hour");
+    now = moment().format('hA');
+    console.log(now)
+     hourBlock.each(function () {
+            hour1 = $(this).text();
+            console.log(hour1)
+                 if (hour1 === now) {
+                     $(this).next().addClass("present")
+                 }
+                 if(moment(hour1,'hA').isAfter(moment())){
+                    $(this).next().addClass("future")
+                 }
+                 if (moment(hour1, 'hA').isBefore(moment())) {
+                    $(this).next().addClass("past")
+                 }
+                  // if (moment(hour1, 'hA') = moment('hA')) {
+                  //      $(this).next().addClass("present")
+                  //  }
+})
 
 }
 
 
+    now = moment().format('hA');
 
+    console.log(moment('1PM', 'hA').isValid());
+    console.log(moment('1PM', 'hA').isBefore(moment()));
 
+    $(".saveBtn").on("click", function (event) {
+        //save textContent from text area to localstorage here
+        event.preventDefault();
 
-
-   $(".saveBtn").on("click", function () {
-       //save textContent from text area to localstorage here
-       var $curr = $("saveBtn");
-
-        // console.log($(this).prev().val()); 
-
+        timeHour = $(this).prev().prev().text();
         textArea = $(this).prev().val();
-
-        timeHour = $(this).prevAll().text();
+        
         
         storeResult();
         // console.log(textArea);
@@ -33,103 +79,36 @@ function renderNotes (){
         // var savedNote = JSON.parse(localStorage.getItem('note')) || [];
         // appendToStorage(savedNote, textArea)
 
-   })
+    })
 
-       
-      
- // function to store text of note and related hour 
- // allows for notes to be edited and saved over existing note.  does not replicate
-    
-   function storeResult(){
+ renderNotes();
 
-   const oldNote = JSON.parse(localStorage.getItem('note')) || [];
-   const hourToUse = timeHour;
-   const existingItem = oldNote.find(({time}) => time === hourToUse);
-   if (existingItem) {
-       Object.assign(existingItem, {
-           'time': timeHour,
-           'text': textArea
-       })
-   } else {
-       const newNote = {
-           'time': hourToUse,
-           'text': textArea
-           
-       };
-       oldNote.push(newNote);
-   }
+    // function to store text of note and related hour 
+    // allows for notes to be edited and saved over existing note.  does not replicate
 
-   localStorage.setItem('note', JSON.stringify(oldNote));
+    function storeResult() {
 
-   }
+        const oldNote = JSON.parse(localStorage.getItem('note')) || [];
+        const hourToUse = timeHour;
+        const existingItem = oldNote.find(({
+            time
+        }) => time === hourToUse);
+        if (existingItem) {
+            Object.assign(existingItem, {
+                'time': timeHour,
+                'text': textArea
+            })
+        } else {
+            const newNote = {
+                'time': hourToUse,
+                'text': textArea
 
+            };
+            oldNote.push(newNote);
+        }
 
+        localStorage.setItem('note', JSON.stringify(oldNote));
 
-
-
-
-
-
-//    function storeResult(){
-
-//     var savedNote = JSON.parse(localStorage.getItem('note')) || [];
-//     localStorage.setItem('note', JSON.stringify(savedNote));
-//     savedNote = 
-//    // function storeResult(){
-        
-
-
-
-
-//         // localStorage.setItem("note", JSON.stringify(oldItems));
-
-//         // var newItem = {
-//         //      time: timeHour,
-//         //      text: textArea
-//         //  };
-//         // var oldItems = JSON.parse(localStorage.getItem("note")) || [];
-
-           
-
-//         // oldItems.push(newItem);
-
-//         // localStorage.setItem("note", JSON.stringify(oldItems));
-//         // }
-
-
-// var yesArray = [];
-// localStorage.setItem('yesArray', JSON.stringify(yesArray));
-// yesArray = JSON.parse(localStorage.getItem('yesArray'));
-// yesArray.push('yes');
-// localStorage.setItem('yesArray', JSON.stringify(yesArray));
-// JSON.parse(localStorage.getItem('yesArray')); // Returns ["yes"]
-
-
-//    function storeResult(){
-             
-//     var savedNote = JSON.parse(localStorage.getItem('note')) || [];
-//                 savedNote.push({
-//                 time: timeHour,
-//                 text: textArea
-//             });
-
-//             savedNote.
-//     localStorage.setItem('note', JSON.stringify(savedNote));
-//    }
-
-  function appendToStorage(note, data) {
-      var old = localStorage.getItem(note.time);
-      if (old === null) old = "";
-      localStorage.setItem(note, old + data);
-  }
-
-//   appendToStorage('oldData', $i("textbox").value);
-
-   function postNotes () {
-
-
-
-
-   }
+    }
 
 });
